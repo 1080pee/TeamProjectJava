4fe29199df27c5f78521cc8fd2e36b0027d79d10
1080pee <peterhuynh90@gmail.com>
2014-10-20 16:23:15 -0400

    Java project
    our project of java

---
+++ src/QuizzApplet.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Peter
+ *
+ */
+public class QuizzApplet {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String fname = "";
+		String lname = "";
+	}
+
+}
